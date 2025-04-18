718f313d1b784df2f0339c455c1857203c22aedf
aiganymshynarbek <113932336+aiganymshynarbek@users.noreply.github.com>
2025-04-18 16:13:11 +0500

    Add files via upload

---
+++ T1/T1/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите первое число:");
+double num1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+double num2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите операцию:");
+char operation = Console.ReadLine()[0];
+
+switch (operation)
+{
+    case '+':
+        Console.WriteLine($"Результат: {num1 + num2}");
+        break;
+    case '-':
+        Console.WriteLine($"Результат: {num1 - num2}");
+        break;
+    case '*':
+        Console.WriteLine($"Результат: {num1 * num2}");
+        break;
+    case '/':
+        if (num2 != 0)
+        {
+            Console.WriteLine($"Результат: {num1 / num2}");
+        }
+        else
+        {
+            Console.WriteLine("Нельзя делить на 0");
+        }
+        break;
+    default:
+        Console.WriteLine("Недопустимая операция. Введите +, -, *, или /.");
+        break;
+}
