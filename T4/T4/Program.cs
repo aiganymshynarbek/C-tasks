cbb7a000d9fc84d9f103da54f9c212b6cc969a3c
aiganymshynarbek <113932336+aiganymshynarbek@users.noreply.github.com>
2025-04-21 12:53:42 +0500

    Add files via upload

---
+++ T4/T4/Program.cs
@@ -0,0 +1,39 @@
+﻿Random random = new Random();
+int [] numbers = new int [10];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    numbers[i] = random.Next(1, 101);
+    Console.Write(numbers[i] + " ");
+}
+
+Console.WriteLine();
+
+int max = numbers[0];
+int min = max;
+double sum = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+    {
+        sum += numbers[i];
+        if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+
+        if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }
+    }
+
+double avg = sum / numbers.Length;
+Console.WriteLine($"Max is: {max}");
+Console.WriteLine($"Min is: {min}");
+Console.WriteLine($"Avg is: {avg}");
+
+
+
+
+
+
