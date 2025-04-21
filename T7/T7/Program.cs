4d2b8c08847239ac4b925d643d2a9f9a443b6810
aiganymshynarbek <113932336+aiganymshynarbek@users.noreply.github.com>
2025-04-21 17:00:49 +0500

    Add files via upload

---
+++ T7/T7/Program.cs
@@ -0,0 +1,28 @@
+﻿class Person
+{
+    public string name;
+    public int age;
+
+    public Person(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void sayHello()
+    {
+        Console.WriteLine("Привет " + name + ", " + age + "!");
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        
+        Console.WriteLine("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Person p = new Person(name, age);
+        p.sayHello();
+    }
+}
